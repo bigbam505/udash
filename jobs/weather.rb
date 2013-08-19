d2a320f9d7d1a8d7a1e71529c59f120b26ca7097
bigbam505 <brent@bmontague.com>
2013-08-19 09:06:29 +0530

    Add a dashboard for trip to india

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -10,6 +10,7 @@ require_relative '../lib/weather_updater'
 cities = []
 cities << {city: 'McLean', woe_id: 2448240}
 cities << {city: 'Arlington', woe_id: 12766856}
+cities << {city: 'Gurgaon', woe_id: 2295020}
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
   cities.each do |city|
