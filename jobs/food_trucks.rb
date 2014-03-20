47573e5e604ec64138c1f01cc218da7e78dc7781
Montague, Brent <bmontague@cvent.com>
2014-03-20 11:25:10 -0400

    Update food truck update Frequency

--- jobs/food_trucks.rb
+++ jobs/food_trucks.rb
@@ -5,7 +5,7 @@ require_relative '../lib/food_truck_updater'
 locations = []
 locations << {location: 'Greensboro', lat: 38.9236427, long: -77.2306207 }
 
-SCHEDULER.every '30m', :first_in => 0 do |job|
+SCHEDULER.every '5m', :first_in => 0 do |job|
   locations.each do |location|
     FoodTruckUpdater.new(location[:location],
                          location[:lat],
